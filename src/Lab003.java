c4dd11620c59844e736ecee3b52b68fcaffbe739
kanakalakshmi <kyklaxmieashwar@gmail.com>
2024-01-02 17:47:58 +0530

    first  3 programs which i wrotw

---
+++ src/Lab003.java
@@ -0,0 +1,10 @@
+package src;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        String name="LAXMI";
+                String x="My name is";
+        System.out.println(x+name);
+
+    }
+}
